103f795f352e1d0510383a3d5e1b12786db6c04e
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-22 12:23:16 -0500

    "metodo map"

--- server/02-map.js
+++ server/02-map.js
@@ -1,8 +1,17 @@
-const letters = ["a", "b", "c"];
-const newArray = letters.map((item) => item + "++");
-console.log("original", letters);
-console.log("new", newArray);
+// const letters = ["a", "b", "c"];
+// const newArray = letters.map((item) => item + "++");
+// console.log("original", letters);
+// console.log("new", newArray);
 
-const cook = ["chicken", "cow", "pig", "cheese"];
-const nexCook = cook.map((item) => item + "Uhm");
-console.log(nexCook);
+// const cook = ["chicken", "cow", "pig", "cheese"];
+// const nexCook = cook.map((item) => item + "Uhm");
+// console.log(nexCook);
+const products = [
+  { title: "Burguer", price: 12 },
+  { title: "Pizza", price: 15 },
+];
+const app = document.getElementById("app");
+const list = products.map((product) => {
+  return ` <li>${product.title} - ${product.price}</li>`;
+});
+app.innerHTML = list.join("");
