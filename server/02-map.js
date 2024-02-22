0d522eab0b141930de66fb3e1f83945134e4d144
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-22 12:13:01 -0500

    "map"

---
+++ server/02-map.js
@@ -0,0 +1,8 @@
+const letters = ["a", "b", "c"];
+const newArray = letters.map((item) => item + "++");
+console.log("original", letters);
+console.log("new", newArray);
+
+const cook = ["chicken", "cow", "pig", "cheese"];
+const nexCook = cook.map((item) => item + "Uhm");
+console.log(nexCook);
