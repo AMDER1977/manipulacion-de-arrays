0324d734362f87b6a29ce1bb3e29c9cbe89d02e3
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 13:19:34 -0500

    "mutables"

--- server/mutable.js
+++ server/mutable.js
@@ -9,6 +9,7 @@ const myProducts = [];
 console.log("products", products);
 console.log("myProducts", myProducts);
 console.log("-".repeat(10));
+
 const productIndex = products.findIndex((item) => item.id === "🍔");
 if (productIndex !== -1) {
   myProducts.push(products[productIndex]);
@@ -17,11 +18,13 @@ if (productIndex !== -1) {
 console.log("products", products);
 console.log("myProducts", myProducts);
 console.log("-".repeat(10));
+
 const productsV2 = [
   { title: "Pizza", price: 121, id: "🍕" },
   { title: "Burger", price: 121, id: "🍔" },
   { title: "Hot cakes", price: 121, id: "🥞" },
 ];
+
 const update = { id: "🥞", changes: { price: 200, description: "delicioso" } };
 const productIndexV2 = productsV2.findIndex((item) => item.id === update.id);
 productsV2[productIndexV2] = {
