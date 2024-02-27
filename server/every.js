28b21a621d5769c65927711ad8baec9f56e457b2
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 10:31:49 -0500

    "every"

---
+++ server/every.js
@@ -0,0 +1,18 @@
+//* todos cumplan con la condicion retorna TRUE OR FALSE
+
+const numbers = [1, 5, 6, 9, 23, 45];
+
+let response = true;
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index];
+  if (element >= 40) {
+    response = false;
+  }
+}
+console.log(response);
+
+//** con every */
+
+let response2 = numbers.every((item) => item <= 40);
+console.log(" Con Every : ", response2);
