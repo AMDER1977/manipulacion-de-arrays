58d8677bd8807590baefdd2847fbe15c3fec7885
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 12:10:15 -0500

    "join+split"

--- server/join.js
+++ server/join.js
@@ -1,4 +1,4 @@
-const elementos = ["fire", "air", "water", "earth"];
+const elementos = ["Fire", "air", "water", "Earth"];
 
 let respuesta = "";
 const separator = "--";
@@ -10,3 +10,10 @@ for (let index = 0; index < elementos.length; index++) {
 console.log("con for: ", respuesta);
 
 console.log("con join:", elementos.join(separator));
+
+//** OTRA APLICACION */
+
+const title = "curso de Manipulacion de Arrays";
+
+const pasandoArray = title.split(" ").join(separator).toLowerCase();
+console.log(pasandoArray);
