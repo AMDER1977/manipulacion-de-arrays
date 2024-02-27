9b3ba3e094bdb4ddc35d09434d523aa17ae371ef
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 12:34:20 -0500

    "usando concatenacion"

--- server/concat.js
+++ server/concat.js
@@ -3,14 +3,18 @@ const otherElements = [3, 3, 4, 4];
 
 //*haciendolo con for tradicional
 
-// let newArray = elements;
-// for (let i = 0; i < otherElements.length; i++) {
-//   const element = otherElements[i];
-//   newArray.push(element);
-// }
-// console.log(newArray);
+let newArray = [...elements];
+for (let i = 0; i < otherElements.length; i++) {
+  const element = otherElements[i];
+  newArray.push(element);
+}
+console.log("Usando el FOR clasico: ", newArray);
 
 //** CON CONCAT */
 
 const conConcat = elements.concat(otherElements);
-console.log(conConcat);
+console.log("usando concat: ", conConcat);
+
+//**IGUAL SE PUEDE UTILIZAR EL SPREAD OPERATION */
+const respuestaN = [...elements, ...otherElements];
+console.log("Con SPREAD : ", respuestaN);
