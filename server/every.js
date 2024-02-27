10e26a5651681674eb4c40c1f5285b5f1522dcda
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 10:48:29 -0500

    "everyreto"

--- server/every.js
+++ server/every.js
@@ -16,3 +16,15 @@ console.log(response);
 
 let response2 = numbers.every((item) => item <= 50);
 console.log(" Con Every : ", response2);
+//* reto
+
+const team = [
+  { name: "Nicolas", age: 12 },
+  { name: "Andrea", age: 8 },
+  { name: "Zulema", age: 2 },
+  { name: "Santiago", age: 18 },
+];
+
+const mayorQue = team.every((item) => item.age < 15);
+
+console.log("Este es el reto veamos: ", mayorQue);
