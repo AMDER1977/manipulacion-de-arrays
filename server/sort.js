efa595de6e58eceab2f9893050c8904b7f041ded
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 13:23:26 -0500

    "ordenamiento"

--- server/sort.js
+++ server/sort.js
@@ -1,9 +1,11 @@
 const months = ["March", "Jan", "Feb", "Dec"];
 months.sort();
 console.log(months);
+
 const numbers = [1, 30, 4, 21, 100000];
 numbers.sort((a, b) => b - a);
 console.log(numbers);
+
 const words = [
   "réservé",
   "premier",
@@ -15,6 +17,7 @@ const words = [
 ];
 words.sort((a, b) => a.localeCompare(b));
 console.log(words);
+
 const orders = [
   { customerName: "Nicolas", total: 600, delivered: true },
   { customerName: "Zulema", total: 120, delivered: false },
