f53e045f2ee3353f42e11794c3a851594d45a76a
Amderson Hernandez <ahernandezdep@gmail.com>
2024-02-27 11:29:29 -0500

    "include for"

--- server/Includes.js
+++ server/Includes.js
@@ -1,11 +1,15 @@
 const pets = ["cat", "dog", "turtle", "fox"];
 
-let petInclude = [];
+//let petInclude = [];//aca lo hice asi pero solo pudiera tener una variable en false
+let petInclude = false;
 
 for (let index = 0; index < pets.length; index++) {
   const element = pets[index];
-  if (element === "turtle") {
-    petInclude.push(element);
+  if (element === "dog") {
+    // petInclude.push(element);// aca pushee pero solo se cambia a true.
+    petInclude = true;
   }
 }
-console.log(petInclude);
+console.log("hecho con for pusheando el elemento encontrado:", petInclude);
+
+//** AHORA CON INCLUDE ES UN BOOLEANO QUE RETORNA */
